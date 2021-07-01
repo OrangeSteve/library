5d5ff04992bf9afd830c64799bd97665d256cc44
Steven Redman <StevenRedman122@gmail.com>
2021-07-01 04:38:59 +0100

    User can now add books to the library and store the library locally.

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
 const mainContent = document.querySelector(`main`);
-const addBookForm = document.querySelector(`#add-book-form`);
-const bookTitleInput=document.querySelector(`#book-title`);
-const bookAuthorInput=document.querySelector(`#book-author`);
-const bookPagesInput=document.querySelector(`#book-pages`);
-const bookReadRadio=document.querySelector(`#book-read`);
-const bookReadingRadio=document.querySelector(`#book-reading`);
-const bookNotReadRadio=document.querySelector(`#book-not-read`);
-const readStatusRadios=document.getElementsByName(`book-read-status`);
+const addBookForm = document.getElementById(`add-book-form`);
+const bookTitleInput = document.querySelector(`#book-title`);
+const bookAuthorInput = document.querySelector(`#book-author`);
+const bookPagesInput = document.querySelector(`#book-pages`);
+const bookReadRadio = document.querySelector(`#book-read`);
+const bookReadingRadio = document.querySelector(`#book-reading`);
+const bookNotReadRadio = document.querySelector(`#book-not-read`);
+const readStatusRadios = document.getElementsByName(`book-read-status`);
 const bookCardHolder = document.createElement(`div`);
 const addBookMenuButton = document.querySelector(`#add-book-button`);
+const submitButton = document.querySelector(`#submit`);
 
-let myLibrary = [];
-let bookCards = [];
+submitButton.addEventListener(`click`, () => {
+    let readStatus;
+    for (let i = 0; i < readStatusRadios.length; i++) {
+        if (readStatusRadios[i].getAttribute("checked") == "checked") {
+            readStatus = readStatusRadios[i].getAttribute("value");
+            break;
+        }
 
+    }
+    const bookToAdd = new Book(bookTitleInput.value, bookAuthorInput.value, bookPagesInput.value, readStatus);
+    addBookToLibrary(bookToAdd);
+    closeAddBookMenu();
+
+});
+
+addBookMenuButton.addEventListener(`click`, openAddBookMenu);
 addBookForm.remove();
+
+let myLibrary = JSON.parse(localStorage.getItem("mylibrary") || "[]");
+console.log(`Number of books: ${myLibrary.length}`);
+myLibrary.forEach(function (book, index) {
+    console.log("[" + index + "]:" + myLibrary[index].title+" by "+myLibrary[index].author);
+});
+
+let bookCards = [];
 bookCardHolder.classList.add(`card-holder`);
 mainContent.appendChild(bookCardHolder);
-addBookMenuButton.addEventListener(`click`, openAddBookMenu);
-
-for (let i = 0; i < 150; i++) {
-    const newBook = new Book(`Book ${i}`, `Mr A.${i}`, `${Math.round(100 + (Math.random() * 950))}`, Math.round(Math.random()));
-    addBookToLibrary(newBook);
-}
-
 refreshBooks();
-
-
 
 function openAddBookMenu() {
     bookCardHolder.remove();
@@ -45,9 +58,9 @@ function closeAddBookMenu() {
 }
 
 
-
 function refreshBooks() {
     hideBooks();
+    saveLibrary();
     showBooks();
 }
 
@@ -58,26 +71,26 @@ function Book(title, author, pages, read) {
     this.read = read;
     this.info = () => {
         let readString;
-        switch (read){
-            case 0:readString="have read.";
-            break;
-            case 1: readString="am currently reading.";
-            break;
-            case 2: readString="have not read."
+        switch (read) {
+            case 0: readString = "have read.";
+                break;
+            case 1: readString = "am currently reading.";
+                break;
+            case 2: readString = "have not read."
         }
-     
+
         return `${title} by ${author}, ${pages} pages,${readString}`;
     }
 }
 
-function BookCard(index,card, title, author, pages, read,removeBook) {
-    this.index=index;
+function BookCard(index, card, title, author, pages, read, removeBook) {
+    this.index = index;
     this.card = card;
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.removeBook=removeBook;
+    this.removeBook = removeBook;
     this.card.appendChild(this.title);
     this.card.appendChild(this.author);
     this.card.appendChild(this.pages);
@@ -100,12 +113,12 @@ function showBooks() {
             bookCards[i].title.textContent = `${myLibrary[i].title}`;
             bookCards[i].author.textContent = `by ${myLibrary[i].author}`;
             bookCards[i].pages.textContent = `${myLibrary[i].pages} pages`;
-            bookCards[i].removeBook.textContent=`Remove from library`;
+            bookCards[i].removeBook.textContent = `Remove from library`;
             bookCards[i].card.classList.add(`book-card`);
-            if (myLibrary[i].read ==0) {
+            if (myLibrary[i].read == 0) {
                 bookCards[i].read.textContent = "Read";
                 bookCards[i].card.classList.add(`read`);
-            }else if(myLibrary[i].read==1){
+            } else if (myLibrary[i].read == 1) {
                 bookCards[i].read.textContent = "Reading";
                 bookCards[i].card.classList.add(`reading`);
 
@@ -114,18 +127,18 @@ function showBooks() {
                 bookCards[i].card.classList.add(`not-read`);
             }
 
-bookCards[i].read.addEventListener(`click`,()=>{
-    myLibrary[i].read++;
-    if (myLibrary[i].read>2){
-        myLibrary[i].read=0;
-    }
-    refreshBooks();
-})
-
-            bookCards[i].removeBook.addEventListener(`click`,()=>{
-                myLibrary.splice(bookCards[i].index,1);
+            bookCards[i].read.addEventListener(`click`, () => {
+                myLibrary[i].read++;
+                if (myLibrary[i].read > 2) {
+                    myLibrary[i].read = 0;
+                }
                 refreshBooks();
-                    });
+            })
+
+            bookCards[i].removeBook.addEventListener(`click`, () => {
+                myLibrary.splice(bookCards[i].index, 1);
+                refreshBooks();
+            });
 
             bookCardHolder.appendChild(bookCards[i].card);
         }
@@ -144,4 +157,40 @@ function hideBooks() {
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+    saveLibrary();
 }
+
+
+
+
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        let x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch (e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+
+function saveLibrary() {
+    localStorage.setItem(`mylibrary`, JSON.stringify(myLibrary));
+}
+
+
+
